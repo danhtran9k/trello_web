c2c60180ea5a59a73e1b22ed71aef158e51648b0
danh tran <tldanh1993@gmail.com>
2022-03-23 12:46:50 +0700

    webkit-scrollbar css

--- src/App.js
+++ src/App.js
@@ -34,6 +34,62 @@ function App() {
           </ul>
           <footer> Add another card</footer>
         </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/902/200/200" alt="img-demo" />
+              Title: Demo img
+            </li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/902/200/200" alt="img-demo" />
+              Title: Demo img
+            </li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/902/200/200" alt="img-demo" />
+              Title: Demo img
+            </li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/902/200/200" alt="img-demo" />
+              Title: Demo img
+            </li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+            <li>Add what todo</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
       </nav>
     </div>
   );
